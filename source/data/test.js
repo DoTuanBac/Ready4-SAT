0f4d2ad609c723016f6935e404c9d562233d26a9
DoTuanBac <43380137+DoTuanBac@users.noreply.github.com>
2020-07-04 22:27:57 +0700

    lên

--- source/data/test.js
+++ source/data/test.js
@@ -20,13 +20,13 @@ export const  Test = [
         {
           id:2,
           title:"bài 1",
-          question:"Joshua believed his bicycle to be the best \nWhat is type 'Believed' word?",
+          question:"2145+17984/2+4x5x6=?",
           mastered:0,
           answers : [
-            {label: "Noun", value: 0 },
-            {label: "Pronoun", value: 1 },
-            {label: "Adjective", value: 2 },
-            {label: "Verb", value: 3 },
+            {label: "11257", value: 0 },
+            {label: "11111", value: 1 },
+            {label: "11725", value: 2 },
+            {label: "25711", value: 3 },
           ],
           correct:0,
           
@@ -34,21 +34,21 @@ export const  Test = [
           {
             id:3,
             title:"bài 1",
-            question:"Joshua believed his bicycle to be the best \nWhat is type 'Believed' word?",
+            question:"Joshua so hoped his bicycle to be the best \nWhat is type 'so' word?",
             mastered:0,
             answers : [
               {label: "Noun", value: 0 },
               {label: "Pronoun", value: 1 },
               {label: "Adjective", value: 2 },
-              {label: "Verb", value: 3 },
+              {label: "Adv", value: 3 },
             ],
-            correct:0,
+            correct:3,
             
             },
             {
               id:4,
               title:"bài 1",
-              question:"Joshua believed his bicycle to be the best \nWhat is type 'Believed' word?",
+              question:"Joshua believed his bicycle to be the best \nWhat is type 'his' word?",
               mastered:0,
               answers : [
                 {label: "Noun", value: 0 },
@@ -56,7 +56,7 @@ export const  Test = [
                 {label: "Adjective", value: 2 },
                 {label: "Verb", value: 3 },
               ],
-              correct:0,
+              correct:2,
               
               },
     ],
